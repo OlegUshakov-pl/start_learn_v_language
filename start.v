7881633ba3584928e7550e9201ad9f7b08e10817
Oleg Ushakov <olegushakov38@gmail.com>
2026-07-24 06:51:31 +0200

    Refactor main function to include user input for addition

--- start.v
+++ start.v
@@ -1,10 +1,26 @@
 import os
 
 fn main() {
-    areas := ['game', 'web', 'tools', 'science', 'systems',
+   start()
+   println('-----------------------')
+   a := os.input('Enter first number: ').int()
+   b := os.input('Enter second number: ').int()
+   add(a,b)
+
+}
+
+
+fn start() {
+     areas := ['game', 'web', 'tools', 'science', 'systems',
               'embedded', 'drivers', 'GUI', 'mobile']
     for area in areas {
         println('Hello, ${area} developers!')
     }
-     os.input('Press Enter to exit...')
+    os.input('Push any key')
+}
+
+fn add(a int, b int){
+    c :=a+b
+    println('The sum of ${a} and ${b} is ${c}')
+    os.input('Push any key')
 }
