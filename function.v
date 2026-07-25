3e2379832c9f5aec2dcd7d9d68f94b2bba74fc35
Oleg Ushakov <olegushakov38@gmail.com>
2026-07-25 08:39:18 +0200

    function

--- function.v
+++ function.v
@@ -1,16 +1,32 @@
 import os
+import math
 
 fn main(){
 
 a_name:= os.input('Enter your name: ')
 b_sur:= os.input('Enter your surname: ')
 
+a:=os.input('Input side a: ').f64()
+b:=os.input('Input side b: ').f64()
+
 par(a_name, b_sur)
+
+multiply(a, b)
+
+os.input('Press Enter to exit...')
 
 }
 
 fn par(a_name string, b_sur string){
-  println('-----------------------')
-	println('Hello ${a_name} ${b_sur}')
-	println('-----------------------')
+println('-----------------------')
+println('Hello ${a_name} ${b_sur}')
+println('-----------------------')
+}
+
+fn multiply(a f64, b f64){
+
+ mul:= math.sqrt(a**2 + b**2)
+
+ println('The result of the multiplication is: ${mul:.2f}')
+
 }
