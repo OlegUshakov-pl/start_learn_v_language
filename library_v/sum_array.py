5290a78a85c03e89ecbeea22b51c3d22d807334e
Oleg Ushakov <olegushakov38@gmail.com>
2026-07-26 18:33:20 +0200

    array

---
+++ library_v/sum_array.py
@@ -0,0 +1,11 @@
+arr=input('Give me array: ')
+
+
+arr=arr.split(',')
+
+
+sum=0
+for sum in arr:
+    sum +=arr[0]
+
+print ('Summ if array:', sum)
---
+++ library_v/sum_array.v
@@ -0,0 +1,17 @@
+import os
+
+fn main(){
+
+mut arr:=os.input("Give mr array: ")
+
+arr_n := arr.split(',')
+
+mut sum:=0
+
+for item in arr_n{
+  sum += item.int()
+}
+
+println('Sum equal: ${sum}')
+
+}
